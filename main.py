dbfbc4e56779ad70f012625b2ba2842fa1409714
Grigorean Alexandru <grigoreanalexandru40@gmail.com>
2026-02-22 10:54:31 -0500

    Pasul 2 :Stergere spatii multiple

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import re
 with open("input.txt" , "r") as file:
 	content = file.read()
 print(content)
+
+text_fara_spatii = re.sub(r'\s+' , ' ',file)
+
+print("Text dupa eliminare spatii : ", text_fara_spatii)
 
